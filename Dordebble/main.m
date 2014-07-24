4a4cf69341b0e5733b56dd3037c5e6e700da7700
Daniel Vela <daniel.vela@hp.com>
2014-07-24 10:17:42 +0200

    Initial Commit

---
+++ Dordebble/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dordebble
+//
+//  Created by Daniel Vela on 24/07/14.
+//  Copyright (c) 2014 veladan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ DordebbleTests/DordebbleTests.m
@@ -0,0 +1,34 @@
+//
+//  DordebbleTests.m
+//  DordebbleTests
+//
+//  Created by Daniel Vela on 24/07/14.
+//  Copyright (c) 2014 veladan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DordebbleTests : XCTestCase
+
+@end
+
+@implementation DordebbleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
